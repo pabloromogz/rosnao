6937117d0383b9fd9c3e5f6977add47c4da7c762
pablo <pr1220@ic.ac.uk>
2023-09-25 17:02:14 +0800

    Added launch file for simutaneous camera and slam

--- run_cam_slam.sh
+++ run_cam_slam.sh
@@ -9,4 +9,4 @@ FRAME="world"
 SCRIPT_DIR=$(dirname `echo $(realpath "$0")`)
 source `echo $SCRIPT_DIR`/devel/setup.bash
 
-roslaunch rosnao_bridge image_relay.launch nao_ip:=`echo $NAO_IP` shm_id:=`echo $SHM_ID` res:=`echo $RES` cam:=`echo $CAM` topic:=`echo $TOPIC` frame:=`echo $FRAME`
+roslaunch rosnao_bridge image_relay_slam.launch nao_ip:=`echo $NAO_IP` shm_id:=`echo $SHM_ID` res:=`echo $RES` cam:=`echo $CAM` topic:=`echo $TOPIC` frame:=`echo $FRAME`
