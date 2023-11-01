a7a5a92c1c1a4a1bd76bf87f02eeddc451b8f13d
pablo <pr1220@ic.ac.uk>
2023-11-01 17:53:17 +0800

    Adjusted orbslam framerate values for nao camera

--- src/rosnao_wrapper/src/image_publisher.cpp
+++ src/rosnao_wrapper/src/image_publisher.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 {
     if (argc != 5)
     {
-        std::cerr << "Wrong arguments for IMAGE_PUB. Usage: nao_ip, shm_id, res {1=QVGA, 2=VGA}, cam {0=top,1=bottom}" << std::endl;
+        std::cerr << "Wrong arguments for IMAGE_PUB. Usage: nao_ip, shm_id, res {1=QVGA, 2=VGA, 3=4VGA}, cam {0=top,1=bottom}" << std::endl;
         // The same shm_id for the same cam cannot be reused. It will cause other publishers to stop working.
         //
         return 1;
