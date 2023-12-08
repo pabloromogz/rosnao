ac7ebe448e3e3ee1a5cd9bd1fdc7db9f5cd5b4ba
pabloromogz <pr1220@ic.ac.uk>
2023-12-08 09:36:32 +0800

    "added run_cam with no rviz updated some parameters in NAO mono .yaml added image relay launch file with no rviz

---
+++ run_cam_no_rviz.sh
@@ -0,0 +1,11 @@
+NAO_IP=192.168.20.156
+SHM_ID=img
+RES=1 #1 for QVGA, 2 for VGA, 3 for 4VGA(1280*960)
+CAM=0 #0 for top, 1 for bottom
+TOPIC="cam0/image_raw"
+FRAME="world"
+
+SCRIPT_DIR=$(dirname `echo $(realpath "$0")`)
+source `echo $SCRIPT_DIR`/devel/setup.bash
+
+roslaunch rosnao_bridge image_relay_no_rviz.launch nao_ip:=`echo $NAO_IP` shm_id:=`echo $SHM_ID` res:=`echo $RES` cam:=`echo $CAM` topic:=`echo $TOPIC` frame:=`echo $FRAME`
